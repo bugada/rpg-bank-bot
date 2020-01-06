b3048553b4b4c3da85210629efc7224b2c0f34a4
Andrea Bugada <andrea.bugada@gmail.com>
2020-01-06 23:58:36 +0100

    Non command messages cause errors
    Extended the chec for message null to the whole process method.
    This avoid errors on message from the group that are not commands.

--- src/Api.php
+++ src/Api.php
@@ -22,38 +22,38 @@ class Api extends \Telegram\Bot\Api {
 	}
 
 	public function getCommandBus() {
-        if (is_null($this->commandBus)) {
-            return $this->commandBus = new CommandBus($this);
-        }
+		if (is_null($this->commandBus)) {
+				return $this->commandBus = new CommandBus($this);
+		}
 
-        return $this->commandBus;
-    }
+		return $this->commandBus;
+	 }
 
 	protected function processCommand(Update $update) {
 		$message = $update->getMessage();
 
-		// Get member status
-		//TODO: use PHPCache and getChatAdministrators(chat_id) for better performances
-		$status = $this->getChatMember([
-			'chat_id' => $message->getChat()->getId(), 
-			'user_id' => $message->getFrom()->getId()
-		])->get('status');
-
-		Log::debug('ChatMemberStatus: ' . $status);
-
-
-		$isForAdmin = $this->getCommandBus()->getCommand($message->getText())->isForAdmin();
-		Log::debug('IsForAdmin: ' . ($isForAdmin ? 'true' : 'false'));
-
-		if ($isForAdmin && $status != 'creator' && $status != 'administrator') {
-			$this->sendMessage([
-				'chat_id' => $message->getChat()->getId(), 
-				'text' => 'this command can be invoked by admin only'
-			]);
-			return;
-		}
-		
 		if ($message !== null && $message->has('text')) {
+
+			// Get member status
+			//TODO: use PHPCache and getChatAdministrators(chat_id) for better performances
+			$status = $this->getChatMember([
+				'chat_id' => $message->getChat()->getId(), 
+				'user_id' => $message->getFrom()->getId()
+			])->get('status');
+
+			Log::debug('ChatMemberStatus: ' . $status);
+
+			$isForAdmin = $this->getCommandBus()->getCommand($message->getText())->isForAdmin();
+			Log::debug('IsForAdmin: ' . ($isForAdmin ? 'true' : 'false'));
+
+			if ($isForAdmin && $status != 'creator' && $status != 'administrator') {
+				$this->sendMessage([
+					'chat_id' => $message->getChat()->getId(), 
+					'text' => 'this command can be invoked by admin only'
+				]);
+				return;
+			}
+
 			$this->getCommandBus()->handler($message->getText(), $update);
 		}
 	}
