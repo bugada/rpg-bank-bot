1ad07379365dba549f26c602752321580da588dd
Andrea Bugada <andrea.bugada@gmail.com>
2020-02-02 23:10:41 +0100

    Better intro fro bot on /start command

--- src/Commands/StartCommand.php
+++ src/Commands/StartCommand.php
@@ -15,6 +15,8 @@ class StartCommand extends \Telegram\Bot\Commands\Command {
 	public function handle($arguments) {
 
 		$text = 'Welcome to RPG Bank' . PHP_EOL . PHP_EOL .
+				  'This bot mimics a bank for role play games ' .
+				  'and can only be used in public or private groups' . PHP_EOL . PHP_EOL .
 				  'To see available commands type /help' . PHP_EOL . PHP_EOL .
 				  'You can view the bot source code at' . PHP_EOL .
 				  'https://github.com/bugada/rpg-bank-bot'; 
